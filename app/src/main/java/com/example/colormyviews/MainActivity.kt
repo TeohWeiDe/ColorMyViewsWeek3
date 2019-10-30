a40ab487ff7cbd3e76b9d488ec9f24abcdabb8cd
TeohWeiDe <teohwd-pm17@student.tarc.edu.my>
2019-10-30 23:05:32 +0800

    commit

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -24,6 +24,15 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_six_text -> view.setBackgroundColor(Color.RED)
+            R.id.box_seven_text -> view.setBackgroundColor(Color.CYAN)
+            R.id.box_eight_text -> view.setBackgroundColor(Color.GREEN)
+            R.id.box_nine_text -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box_ten_text -> view.setBackgroundColor(Color.RED)
+            R.id.box_eleven_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_twelve_text -> view.setBackgroundColor(Color.CYAN)
+            R.id.box_thridteen_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_fourteen_text -> view.setBackgroundColor(Color.RED)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -34,13 +43,25 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val boxSixText = findViewById<TextView>(R.id.box_six_text)
+        val boxSevenText = findViewById<TextView>(R.id.box_seven_text)
+        val boxEightText = findViewById<TextView>(R.id.box_eight_text)
+        val boxNineText = findViewById<TextView>(R.id.box_nine_text)
+        val boxTenText = findViewById<TextView>(R.id.box_ten_text)
+        val boxElevenText = findViewById<TextView>(R.id.box_eleven_text)
+        val boxTwelveText = findViewById<TextView>(R.id.box_twelve_text)
+        val boxThridteenText = findViewById<TextView>(R.id.box_thridteen_text)
+        val boxFourteenText = findViewById<TextView>(R.id.box_fourteen_text)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+                boxFourText, boxFiveText, boxSixText,
+                boxSevenText, boxEightText, boxNineText,
+                boxTenText, boxElevenText, boxTwelveText,
+                boxThridteenText, boxFourteenText, rootConstraintLayout
             )
 
         for (item in clickableViews) {
